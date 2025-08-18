ca7791e308e52c79e9ba3880fee6c1584e430cbd
HuangCaiPing <2227874784@qq.com>
2025-08-18 16:23:31 +0800

    更换中英文图标

--- src/use/lang.ts
+++ src/use/lang.ts
@@ -1,7 +1,6 @@
 export const useLang = () => {
   console.log(localStorage.getItem('lang'))
   const lang = localStorage.getItem('lang') || (navigator.language.includes('zh') ? 'zh' : 'en')
-  console.log('use lang', lang)
   const isZh = lang === 'zh'
   const setLang = (lang) => {
     localStorage.setItem('lang', lang)
